650731eb3043c9c2e9e8e5b2e97bd9fced34060b
houiji20 <amine.houiji20@gmail.com>
2021-10-05 05:30:39 -0700

    test2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main"
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	int i;
 	char c;
