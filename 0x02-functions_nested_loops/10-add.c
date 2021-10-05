b7b3ce6e4fc9fc9222651664c587b7c537cd5cb3
houiji20 <amine.houiji20@gmail.com>
2021-10-05 12:38:19 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+ #include "main.h"
+
+/**
+ * add - add two numbers together
+ * @a: num a to add
+ * @b: num b to add
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+		return (a + b);
+}
+
