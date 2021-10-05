9fa8f5488bc160bd76525e45e47522a1691ecab6
houiji20 <amine.houiji20@gmail.com>
2021-10-05 02:31:21 -0700

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; ++c)
+	printf("%c ", c);
+	printf("\n");
+
+	return (0);
+}
