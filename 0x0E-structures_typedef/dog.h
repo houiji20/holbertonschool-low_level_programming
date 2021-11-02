ba13822be5f8ed3e328d09ab089e7646d3857fc3
root <root@DESKTOP-9JLJ2RU.localdomain>
2021-11-02 19:56:41 +0100

    e

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <dog.h>
+
+struct dog 
+{
+    char *name;
+    char *owner;
+    int age
+};
+int main(void)
+{
+    struct dog my_dog;
+    my_dog.name = "Poppy"
+    my_dog.name = "3.5"
+    my_dog.name = "Bob"
+return (0);
+}
